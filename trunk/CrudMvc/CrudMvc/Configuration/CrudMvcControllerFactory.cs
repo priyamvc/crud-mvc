ed93cf4b559d2e0ad26dac60705ec21f948d1806
bit0102@iit.du.ac.bd <bit0102@iit.du.ac.bd@1fad8f52-bad1-5f1e-f454-6e0c99624641>
2012-06-22 17:26:11 +0000

    The first commint

---
+++ trunk/CrudMvc/CrudMvc/Configuration/CrudMvcControllerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CrudMvc.Controller;
+using System.Linq;
+
+namespace CrudMvc.Configuration {
+    public class CrudMvcControllerFactory : DefaultControllerFactory {
+        private readonly Assembly _entityAssembly;
+        public CrudMvcControllerFactory(Assembly entityAssembly) {
+            _entityAssembly = entityAssembly;
+        }
+
+        protected override Type GetControllerType(RequestContext requestContext, string controllerName) {
+            Type controllerType = base.GetControllerType(requestContext, controllerName);
+
+            try {
+                if(controllerType == null) {
+                    if(controllerName.EndsWith("Crud")) {
+                        string entityName = controllerName.Substring(0, controllerName.Length - 4);
+                        Type entityType = _entityAssembly.GetTypes().First(type => type.Name == entityName);
+                        controllerType = typeof(CrudController<>).MakeGenericType(entityType);
+                    }
+                }
+            } catch { }
+
+
+            return controllerType;
+        }
+    }
+}
