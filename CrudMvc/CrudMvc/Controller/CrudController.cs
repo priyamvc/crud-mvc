cb5c9caa5c145b568e4491a9de097894af590541
bit0102 <bit0102@iit.du.ac.bd>
2012-06-22 17:26:11 +0000

    The first commint

---
+++ CrudMvc/CrudMvc/Controller/CrudController.cs
@@ -0,0 +1,55 @@
+﻿using System.Web.Mvc;
+using CrudMvc.Repository;
+
+namespace CrudMvc.Controller {
+    public class CrudController<TEntity> : System.Web.Mvc.Controller where TEntity : class {
+        protected CrudRepository<TEntity> Repository { get; private set; }
+
+        public CrudController(CrudRepository<TEntity> repository) {
+            Repository = repository;
+        }
+
+        public ActionResult Index() {
+            var models = Repository.GetAll();
+
+            return View(models);
+        }
+
+        public ActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(TEntity entity) {
+            if(ModelState.IsValid) {
+                Repository.Create(entity);
+                return RedirectToAction("Index");
+            }
+
+            return View(entity);
+        }
+
+        public ActionResult Edit(int id) {
+            TEntity entity = Repository.Get(id);
+
+            return View(entity);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TEntity entity) {
+            if(ModelState.IsValid) {
+                Repository.Edit(entity);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(entity);
+        }
+
+        public ActionResult Details(int id) {
+            TEntity entity = Repository.Get(id);
+
+            return View(entity);
+        }
+    }
+}
---
+++ CrudMvc/CrudMvc/Repository/CrudRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+
+namespace CrudMvc.Repository {
+    public class CrudRepository<TEntity> where TEntity : class {
+        protected DbContext Context;
+        public CrudRepository(DbContext context) {
+            Context = context;
+        }
+
+        public TEntity Get(object id) {
+            return Set.Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll() {
+            return Set;
+        }
+
+        public TEntity Create(TEntity entity) {
+            Set.Add(entity);
+            Context.SaveChanges();
+            return entity;
+        }
+
+        public TEntity Edit(TEntity entity) {
+            Context.Entry(entity).State = EntityState.Modified;
+            Context.SaveChanges();
+            return entity;
+        }
+
+        public TEntity Delete(object id) {
+            TEntity entity = Get(id);
+            return Delete(entity);
+        }
+
+        public TEntity Delete(TEntity entity) {
+            Set.Remove(entity);
+            Context.SaveChanges();
+            return entity;
+        }
+
+        protected DbSet<TEntity> Set {
+            get { return Context.Set<TEntity>(); }
+        }
+    }
+}
