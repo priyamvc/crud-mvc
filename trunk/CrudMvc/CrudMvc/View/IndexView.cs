5533b7f5ba38e416d1044e61d5ed8d33c714b49d
bit0102@iit.du.ac.bd <bit0102@iit.du.ac.bd@1fad8f52-bad1-5f1e-f454-6e0c99624641>
2012-06-26 18:14:36 +0000

    Dependenci Injection moved to the library from user code. The user now need to configure somethings in web.config

---
+++ trunk/CrudMvc/CrudMvc/View/IndexView.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CrudMvc.View
+{
+    public class IndexView : IView
+    {
+        #region Implementation of IView
+
+        public void Render(ViewContext viewContext, TextWriter writer)
+        {
+            var models = (IEnumerable<object>)viewContext.ViewData.Model;
+
+            HtmlTextWriter htmlWritter = new HtmlTextWriter(writer);
+            var table = TableForModels(models);
+            table.RenderControl(htmlWritter);
+        }
+
+        #endregion
+
+        private Table TableForModels(IEnumerable<object> models)
+        {
+            Table indexTable = new Table();
+            Type modelType = models.GetType().GetGenericArguments()[0];
+
+            indexTable.Rows.Add(HeadForModelType(modelType));
+            foreach (var model in models)
+            {
+                indexTable.Rows.Add(RowForModel(model));
+            }
+
+            return indexTable;
+        }
+
+        private TableRow HeadForModelType(Type modelType)
+        {
+            TableHeaderRow header = new TableHeaderRow();
+            var properties = modelType.GetProperties();
+
+            foreach (var property in properties)
+            {
+                TableHeaderCell th = new TableHeaderCell {Text = property.Name};
+                header.Cells.Add(th);
+            }
+
+            return header;
+        }
+
+        private TableRow RowForModel(object model)
+        {
+            TableRow row = new TableRow();
+            var properties = model.GetType().GetProperties();
+            foreach (var property in properties)
+            {
+                TableCell cell = new TableCell { Text = property.GetValue(model, null).ToString() };
+                row.Cells.Add(cell);
+            }
+
+            return row;
+        }
+    }
+}
